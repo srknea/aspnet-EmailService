9a2c3cd8ea6d9288da4216c633e537972c885793
Serkan IŞIK <dehnas79@gmail.com>
2023-09-06 22:57:00 +0300

    Update EmailController

--- MailApp.API/Controllers/EmailController.cs
+++ MailApp.API/Controllers/EmailController.cs
@@ -5,6 +5,7 @@ using MailApp.Core.Services;
 using MailApp.Service.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace MailApp.API.Controllers
 {
@@ -21,6 +22,28 @@ namespace MailApp.API.Controllers
             _mapper = mapper;
         }
 
+        // GET : api/email
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var emailAddress = await _emailAddressService.GetAllAsync();
+            var emailAddressDto = _mapper.Map<List<EmailAddressDto>>(emailAddress.ToList());
+
+            return Ok(emailAddressDto);
+        }
+
+        // GET : api/email/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+
+            var emailAddress = await _emailAddressService.GetByIdAsync(id);
+
+            var emailAddressDto = _mapper.Map<EmailAddressDto>(emailAddress);
+
+            return Ok(emailAddressDto);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Save(EmailAddressDto emailAddressDto)
         {
@@ -28,7 +51,25 @@ namespace MailApp.API.Controllers
             var dto = _mapper.Map<EmailAddressDto>(emailAddress);
 
             return Ok(dto);
-            // 201 : Oluşturuldu anlamında kullanılır. İşlem başarılı ise 201 döndürülebilir.
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(EmailAddressDto emailAddressDto)
+        {
+            await _emailAddressService.UpdateAsync(_mapper.Map<EmailAddress>(emailAddressDto));
+
+            return Ok();
+        }
+
+        // DELETE : api/email/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var emailAddress = await _emailAddressService.GetByIdAsync(id);
+
+            await _emailAddressService.RemoveAsync(emailAddress);
+
+            return Ok();
         }
 
     }
